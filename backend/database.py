ca591288a27fa0ee6c18394bb102e5903a77c2e7
Mehmet Akif SARI <mehmetakifsari53@gmail.com>
2026-02-20 01:58:00 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,12 +3,4 @@ import os
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-mongo_timeout_ms = int(os.environ.get('MONGO_TIMEOUT_MS', '5000'))
-
-client = AsyncIOMotorClient(
-    mongo_url,
-    serverSelectionTimeoutMS=mongo_timeout_ms,
-    connectTimeoutMS=mongo_timeout_ms,
-    socketTimeoutMS=mongo_timeout_ms,
-)
-db = client[os.environ['DB_NAME']]
+client = AsyncIOMotorClient(mongo_url)
